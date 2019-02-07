d089306f5574cea7d2f75eb302562ac6dd9c2633
Rick Daglio <rdaglio@Ricks-MacBook-Pro.local>
2019-02-07 16:55:23 -0500

    Added class for managing UserDefaults

---
+++ UserDefaultsManager.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+class UserDefaultsManager {
+    
+    static let openingBalanceKey = "openingBalance"
+    static let expandedFieldsKey = "expandedFields"
+    static let preventSleepKey = "preventSleep"
+    static let haptikKey = "haptik"
+    static let soundsKey = "sounds"
+    
+    static var openingBalance : String {
+        set { UserDefaults.standard.set(newValue, forKey: openingBalanceKey) }
+        get { return UserDefaults.standard.string(forKey: openingBalanceKey)! }
+    }
+    static var expandedFields : Bool {
+        set { UserDefaults.standard.set(newValue, forKey: expandedFieldsKey) }
+        get { return UserDefaults.standard.bool(forKey: expandedFieldsKey) }
+    }
+    static var preventSleep : Bool {
+        set { UserDefaults.standard.set(newValue, forKey: preventSleepKey) }
+        get { return UserDefaults.standard.bool(forKey: preventSleepKey) }
+    }
+    static var haptik : Bool {
+        set { UserDefaults.standard.set(newValue, forKey: haptikKey) }
+        get { return UserDefaults.standard.bool(forKey: haptikKey) }
+    }
+    static var sounds : Bool {
+        set { UserDefaults.standard.set(newValue, forKey: soundsKey) }
+        get { return UserDefaults.standard.bool(forKey: soundsKey) }
+    }
+}
